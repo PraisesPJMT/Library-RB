301b559ffbea18baa3addb6c8597905e0ebc5c0f
PraisesPJMT <praisesmusa@outlook.com>
2022-11-02 14:46:38 +0100

    Create relationship between `Person` and `Rental` class

--- ruby/person.rb
+++ ruby/person.rb
@@ -1,6 +1,6 @@
 require_relative 'nameable'
 class Person < Nameable
-  attr_accessor :name, :age, :parent_permission
+  attr_accessor :name, :age, :parent_permission, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -9,6 +9,7 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
