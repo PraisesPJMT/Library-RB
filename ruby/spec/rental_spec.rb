8849fb274369c916ddd3ea4be17bc2e01a803a14
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-11-09 18:47:16 +0200

    Update-tests

--- ruby/spec/rental_spec.rb
+++ ruby/spec/rental_spec.rb
@@ -1,31 +1,31 @@
-# require_relative 'spec_helper'
+require_relative 'spec_helper'
 
-# describe Rental do
-#   before :each do
-#     @rentals = Rental.new('2020/02/02', 'Jake', 'Harry porter')
-#   end
+describe Rental do
+  before :each do
+    @rentals = Rental.new('2020/02/02', Teacher.new('Math', 30), Book.new('Title', 'Author'))
+  end
 
-#   describe '#new' do
-#     it 'takes three parameters and returns a Rental object' do
-#       expect(@rentals).to be_an_instance_of Rental
-#     end
-#   end
+  describe '#new' do
+    it 'takes three parameters and returns a Rental object' do
+      expect(@rentals).to be_an_instance_of Rental
+    end
+  end
 
-#   describe '#date' do
-#     it 'returns the correct date' do
-#       expect(@rentals.date).to eql '2020/02/02'
-#     end
-#   end
+  describe '#date' do
+    it 'returns the correct date' do
+      expect(@rentals.date).to eql '2020/02/02'
+    end
+  end
 
-#   describe '#person' do
-#     it 'returns the correct person' do
-#       expect(@rentals.person).to be_kind_of String
-#     end
-#   end
+  describe '#person' do
+    it 'should return teacher object' do
+      expect(@rentals.person).to be_an_instance_of Teacher
+    end
+  end
 
-#   describe '#book' do
-#     it 'returns the correct book' do
-#       expect(@rentals.book).to eq 'Harry porter'
-#     end
-#   end
-# end
+  describe '#person' do
+    it 'should return book object' do
+      expect(@rentals.book).to be_an_instance_of Book
+    end
+  end
+end
